a3fae8cdc74d3c8e0d7c47b3bbe58db41f645ce2
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-24 16:58:54 +0530

    useRef with style and fous

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
 import "./App.css";
 
 function App() {
   const [bgColor, setBgcolor] = useState("teal");
   const [isFocus, setIsFocus] = useState(false);
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState    (0);
+
+  const myDivRef = useRef();
+  const myInputref = useRef();
 
   const changeColor = () => {
-    setBgcolor("green");
-    setIsFocus(true);
+    // setBgcolor("green");
+    // setIsFocus(true);
+    myDivRef.current.style.backgroundColor = 'red'
+    myInputref.current.focus()
+    console.log(myDivRef.current)
   };
 
-  console.log(isFocus)
+  console.log("first")
+//  console.log(isFocus)
   return (
     <div
       className="app"
@@ -27,17 +34,18 @@ function App() {
         style={{
           width: "100px",
           height: "100px",
-          backgroundColor: bgColor,
+          //backgroundColor: bgColor,
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           fontSize: '20px',
           fontWeight:'bold'
         }}
+        ref={myDivRef}
       >
         {count}
       </div>
-      <input type=" text" autoFocus={isFocus} />
+      <input type=" text" autoFocus={isFocus} ref={myInputref} />
       <input type=" text" />
       <button onClick={changeColor}>Change Color</button>
       <button onClick={()=>{setCount(count+1)}}>Count +</button>
